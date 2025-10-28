71883f57b6377c240d7f1247f4837931d2293dd6
shahd <shahdadawi301@gmail.com>
2025-10-28 20:44:08 +0200

    navbar component

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import style from './navbar.module.css'
+
+
+function Navbar() {
+
+    return <nav className={`${style.nav} navbar navbar-expand-lg navbar-dark`}>
+        <div className="container">
+            <a className="navbar-brand fw-bold text-uppercase fs-3" href="#">
+                Start Bootstrap
+            </a>
+
+            <div className="ms-auto d-flex gap-4">
+                <a className="nav-link text-white fw-bold text-uppercase fs-6" href="#">
+                    Portfolio
+                </a>
+                <a className="nav-link text-white fw-bold text-uppercase fs-6" href="#">
+                    About
+                </a>
+                <a className="nav-link text-white fw-bold text-uppercase fs-6" href="#">
+                    Contact
+                </a>
+            </div>
+        </div>
+    </nav>
+
+
+}
+
+export default Navbar;
